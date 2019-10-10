47069829228bbd2dba80839fa8b4a886e7892e70
Jess Rezac <jess.rezac@gmail.com>
2019-10-09 19:35:47 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,22 @@
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: proc { |attributes| attributes[:name].blank? }
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
+  def commenters
+    self.comments.map do |comment|
+      comment.user
+    end.uniq
+  end
 
 
 end
